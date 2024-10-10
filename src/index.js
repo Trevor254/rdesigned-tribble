700686d6932954c446a72ad0c3ec34b68078862f
trevor <trevoromondi607@gmail.com>
2024-10-10 23:56:08 +0300

    load DOM content, fetch from server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Your code here
+// Load  the DOM content
+document.addEventListener("DOMContentLoaded", () => {
+    // Fetch data from the local server.
+    fetchAnimals();
+  });
+  // Fetch http://localhost:300/characters.
+  function fetchAnimals() {
+    fetch("http://localhost:3000/characters")
+      .then((resp) => resp.json())
+      .then(myCharacter);
+  }
+  // Takes in a number of characters but only renders them one by one
+  function myCharacter(characters) {
+    characters.forEach(myDetails);
+  }
