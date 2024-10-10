17145cec75f21e963e5896688e0c71059e709acf
trevor <trevoromondi607@gmail.com>
2024-10-10 23:57:04 +0300

    added functionalities

--- src/index.js
+++ src/index.js
@@ -14,3 +14,61 @@ document.addEventListener("DOMContentLoaded", () => {
   function myCharacter(characters) {
     characters.forEach(myDetails);
   }
+
+  // Render animals.
+  let myFlatacutie;
+  function myDetails(character) {
+    const characterId = character.id;
+    // const characterName= character.characterName
+    const characterImage = character.image;
+    const characterVotes = character.votes;
+    const bar = document.querySelector("#character-bar");
+    const spanDetails = document.createElement("span");
+    spanDetails.innerHTML = character.name;
+    // Append spanDetails.
+    bar.appendChild(spanDetails);
+    spanDetails.style.cursor = "pointer";
+    spanDetails.addEventListener("click", () => {
+      // Store the state.
+      myFlatacutie = character;
+      // Show data that is specific to a given animal.
+      showFlatacutie(character);
+    });
+  }
+
+  // Display the selected data.
+  function showFlatacutie(character) {
+    const characterName = document.querySelector("p#name");
+    characterName.innerHTML = character.name;
+    const characterImg = document.querySelector("img#image");
+    characterImg.src = character.image;
+  // Access the votes form.
+    const characterVotes = document.querySelector("span#vote-count");
+    characterVotes.innerHTML = character.votes;
+  }
+  
+  // Reset button.
+  const resetVotes = document.querySelector("button#reset-btn");
+  // Create a pointer. 
+  resetVotes.style.cursor = "pointer";
+  // Add an event listener that fires when the user clicks a button.
+  // Set default animal votes to 0.
+  resetVotes.addEventListener("click", () => {
+    myFlatacutie.votes = 0;
+    showFlatacutie(myFlatacutie);
+    // Reset the form's default values.
+    form.reset();
+  });
+  // Adding votes.
+   const inputvotes = document.querySelector("input#votes");
+  const animalVotes = document.querySelector("span#vote-count");
+  const form = document.querySelector("form#votes-form");
+  // Capture the submit event.
+  form.addEventListener("submit", (e) => {
+    // Prevent the event's default action from occuring.
+    e.preventDefault();
+  //   Return the value provided and display the results together with the animal's details.
+  myFlatacutie.votes += parseInt(e.target.votes.value, 10);
+  showFlatacutie(myFlatacutie);
+    form.reset();
+  });
